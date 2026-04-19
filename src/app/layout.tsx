481c1fb20a59adb2cd3a3a0d993f4ea7f1d7971e
quddus <quddus@localhost.localdomain>
2026-04-19 18:31:41 -0400

    feat: redirect root to /en, add suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="h-full bg-burgundy-900 text-base antialiased">
+    <html lang="en" suppressHydrationWarning className="h-full bg-burgundy-900 text-base antialiased">
       <head>
         <script defer data-domain="winnipeg.pages.dev" src="https://stats.ridvan.org/js/script.js" />
       </head>
